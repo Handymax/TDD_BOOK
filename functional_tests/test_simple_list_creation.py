3783b22cf9d7c736774cb7fbf2205c62f1735d87
guodb <qicai21@gmail.com>
2019-05-26 05:14:06 +0800

    use form in all views, back to working state

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -31,7 +31,7 @@ class NewVistorTest(FunctionalTest):
         # enters "Use peacock feathers to make a fly"
         # (Edith is very methodical)
         inputbox = self.wait_for(lambda: self.browser.find_element_by_id(
-            'id_new_item'
+            'id_text'
             ))
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
