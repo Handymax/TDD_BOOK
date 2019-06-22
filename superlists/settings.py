c9f1b9c78d8bfd9b228e945dfce0d25ee02385f5
guodb <qicai21@gmail.com>
2019-06-23 01:09:49 +0800

    Custom passwordless auth backend + custom user model

--- superlists/settings.py
+++ superlists/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
 ]
 
 AUTH_USER_MODEL = 'accounts.User'
+AUTHENTICATION_BACKENDS = [
+    'accounts.authentication.PasswordlessAuthenticationBackend',
+]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -138,3 +141,10 @@ LOGGING = {
     },
     'root': {'level': 'INFO'},
 }
+
+
+EMAIL_HOST = 'smtp-mail.outlook.com'
+EMAIL_HOST_USER = 'qicai21@hotmail.com'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
